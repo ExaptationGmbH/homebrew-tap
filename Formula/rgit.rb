c27290d5767d634365878bc6fd81406dd4079e2a
lassediercks <1765584+lassediercks@users.noreply.github.com>
2026-07-24 14:59:14 +0000

    chore: bump rgit to 0.0.3

--- Formula/rgit.rb
+++ Formula/rgit.rb
@@ -7,28 +7,28 @@
 class Rgit < Formula
   desc "Run a git command across every repo beneath the current directory"
   homepage "https://github.com/ExaptationGmbH/rgit"
-  version "0.0.2"
+  version "0.0.3"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/ExaptationGmbH/rgit/releases/download/v#{version}/rgit-v#{version}-darwin-arm64"
-      sha256 "0dd3dd84f01afdace7d19f5244ba9171473d8564d9e6c970031e6245024ceb61"
+      sha256 "92767770b43ce847d86f7aff5bfb97a4099be1b092b87c2c41c506ea24245aac"
     end
     on_intel do
       url "https://github.com/ExaptationGmbH/rgit/releases/download/v#{version}/rgit-v#{version}-darwin-amd64"
-      sha256 "3ca14b31369ad166ac59e2b6e1fc64a9a4f00777ebd31dd443e4783d51ade06f"
+      sha256 "dafbf223aaada88cdde34163b27ae6053ea3c1b7f83e7977a2c1022e9788c7be"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/ExaptationGmbH/rgit/releases/download/v#{version}/rgit-v#{version}-linux-arm64"
-      sha256 "923497bcd0945f9731b8f272bc1c7f44beaac25513c14bf4f7c5e3679ef7779c"
+      sha256 "a56d10a1c4f3d4fb282516ad24984654c34a8bf9a5324637623627f08dadea04"
     end
     on_intel do
       url "https://github.com/ExaptationGmbH/rgit/releases/download/v#{version}/rgit-v#{version}-linux-amd64"
-      sha256 "133319e2ec3c3d4f320d7507c2c6eec2ea5d8ea61650e61233352d7bc4f8308d"
+      sha256 "fe349ed1efc63001d41e8b9c3c420ba43dabac2223a654d287bdb6c4f3efe8ea"
     end
   end
 
